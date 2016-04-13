469770a9076968061cdef0b1354b83dccea36ba8
linwenjun <linwenjun@163.com>
2016-04-13 18:00:02 +0800

    remove useless file

--- assembly/docker/shell-scripts/start-mongod.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-[ -e /data/db/mongod.lock ] && rm /data/db/mongod.lock
-mongod --dbpath=/data/db --fork --logpath=/var/log/mongodb.log
--- assembly/mysql/create-db.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mysql -u root -pthoughtworks -e 'CREATE DATABASE IF NOT EXISTS `BronzeSword` CHARACTER SET utf8 COLLATE utf8_general_ci;'
