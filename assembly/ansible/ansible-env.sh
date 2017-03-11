79565b850abd778f5862688e4849f59544edf3af
wyb1995 <wy_b1995@163.com>
2017-03-11 17:03:51 +0800

    modify local.yml

--- assembly/ansible/ansible-env.sh
+++ assembly/ansible/ansible-env.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+rm local-inventory
 cd ../../
 root=$(pwd)
-echo -e "[docker_host] \nlocalhost ansible_connection=local project_root=${root}/" > './assembly/ansible/local-inventory'
+echo -e "[docker_host] \nlocalhost ansible_connection=local project_root=${root}/\n" >> './assembly/ansible/local-inventory'
+echo -e "[local_server] \nlocalhost ansible_connection=local project_root=${root}/\n" >> './assembly/ansible/local-inventory'
