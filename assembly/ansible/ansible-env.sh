fb1bee6394c81e876760ffe19baf22d58972f92d
wyb1995 <wy_b1995@163.com>
2017-03-11 17:20:33 +0800

    modify ansible-env.sh

--- assembly/ansible/ansible-env.sh
+++ assembly/ansible/ansible-env.sh
@@ -4,3 +4,4 @@ cd ../../
 root=$(pwd)
 echo -e "[docker_host] \nlocalhost ansible_connection=local project_root=${root}/\n" >> './assembly/ansible/local-inventory'
 echo -e "[local_server] \nlocalhost ansible_connection=local project_root=${root}/\n" >> './assembly/ansible/local-inventory'
+echo -e "[REMOTE_server] \n54.222.191.27 ansible_ssh_user=ubuntu project_root=${root}/\n" >> './assembly/ansible/local-inventory'
